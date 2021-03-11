89507000e625572dd18c0cb55a7b0457e0dffb88
Whashi <whashi88@gmail.com>
2021-03-11 06:03:18 -0700

    added path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const path = require('path');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
